43c1d686a7774ae821cb988a3d5fc54267960486
Sanketgitbhub <sanketsghotekar2004@gmail.com>
2025-11-04 17:18:06 +0530

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// Typing animation
+var typed = new Typed(".typing-text", {
+  strings: ["Frontend Developer", "Web Designer", "Freelancer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+});
